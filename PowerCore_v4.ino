45879c6d6fc1a68d141429bdc2149894f54fd56b
EdwinIngJ <47252149+EdwinIngJ@users.noreply.github.com>
2019-07-26 18:23:31 -0500

    Update PowerCore_v4.ino

--- PowerCore_v4.ino
+++ PowerCore_v4.ino
@@ -1,7 +1,7 @@
 /*
   Code for running the touchscreen and sensor components used in the power bank project named PowerCore. 
   It contains the drivers for the screen, as well as, the UI for accessing the Voltage, Current, Power, Capacity, and Time to Charge. 
-  This also includes the functions for the collecting and displaying values from a INA219 current sensor.
+  This also includes the functions for the collecting and displaying values from an INA219 current sensor.
 */
 //Libraries
 // Adafruit GFX Library - Version: 1.5.3
