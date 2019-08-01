e4327dd0504de2c2d35aa3a36af50dc55b2224a2
EdwinIngJ <47252149+EdwinIngJ@users.noreply.github.com>
2019-08-01 12:59:35 -0500

    Update PowerCore_v4.ino

--- PowerCore_v4.ino
+++ PowerCore_v4.ino
@@ -299,8 +299,8 @@ void Xtransmission()
 {
  Serial1.print(volt);
  Serial1.print(mA);
- Serial1.print(mV);
+ Serial1.print(mW);
  Serial1.print(mWh);
- Serial1.print(TimeLeft);
+ Serial1.print(Time_Left);
   
 }
